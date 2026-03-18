5582836935028d1431ea27db83c56ab4dba839d2
zacharyb12 <boualemzachary89@yahoo.fr>
2026-03-18 15:19:14 +0100

    Formulaires demo

---
+++ BlazorDemoSap1/BlazorDemoSap1.Client/Models/AddProduct.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDemoSap1.Client.Models
+{
+    public class AddProduct
+    {
+        [Required(ErrorMessage ="Le nom est requis")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Le champ description est requis")]
+        [MaxLength(20,ErrorMessage ="La taille maximum est de 20 caractères")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage ="Le prix est requis")]
+        [Range(1,10000,ErrorMessage ="Le prix doit etre entre 1 et 10000")]
+        public double Price { get; set; }
+
+    }
+}
