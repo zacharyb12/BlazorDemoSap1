d6141fe6767dc52c3c3fedf948f5651ed5539936
zacharyb12 <boualemzachary89@yahoo.fr>
2026-03-18 11:18:30 +0100

    demo binding

--- BlazorDemoSap1/BlazorDemoSap1.Client/Models/Product.cs
+++ BlazorDemoSap1/BlazorDemoSap1.Client/Models/Product.cs
@@ -2,7 +2,7 @@
 {
     public class Product
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
 
         public string Name { get; set; }
 
--- BlazorDemoSap1/BlazorDemoSap1.Client/Pages/Binding.razor.cs
+++ BlazorDemoSap1/BlazorDemoSap1.Client/Pages/Binding.razor.cs
@@ -4,11 +4,12 @@ namespace BlazorDemoSap1.Client.Pages
 {
     public partial class Binding
     {
+        // liste pour l'affichage dans la page
         public List<Product> products = new()
         {
             new Product
             {
-                Id = Guid.Parse("23a29494-d1aa-42fd-9a9a-cfc1ff55a550"),
+                Id = 1,
                 Name = "Apple iPhone 14 Pro Max",
                 Description = "The iPhone 14 Pro Max is the latest flagship smartphone from Apple, featuring a stunning design, powerful performance, and advanced camera capabilities. With its large display, long battery life, and cutting-edge technology, the iPhone 14 Pro Max offers an exceptional user experience for those seeking the best in mobile technology.",
                 Category = "Smartphones",
@@ -17,7 +18,7 @@ namespace BlazorDemoSap1.Client.Pages
             },
             new Product
             {
-                Id = Guid.Parse("5734969e-5048-47db-bb35-a5a785e71fea"),
+                Id = 2,
                 Name = "Samsung Galaxy S23 Ultra",
                 Description = "The Samsung Galaxy S23 Ultra is a high-end smartphone that combines sleek design with powerful performance. It features a large, vibrant display, advanced camera system, and long-lasting battery life. With its cutting-edge technology and innovative features, the Galaxy S23 Ultra offers an exceptional user experience for those seeking the best in mobile technology.",
                 Category = "Smartphones",
@@ -26,7 +27,7 @@ namespace BlazorDemoSap1.Client.Pages
             },
             new Product
             {
-                Id = Guid.Parse("43c5559e-2ef2-4fa9-b608-f5fa4f9862d2"),
+                Id = 3,
                 Name = "Google Pixel 7 Pro",
                 Description = "The Google Pixel 7 Pro is a premium smartphone that offers a sleek design, powerful performance, and advanced camera capabilities. With its large display, long battery life, and cutting-edge technology, the Pixel 7 Pro provides an exceptional user experience for those seeking the best in mobile technology.",
                 Category = "Smartphones",
@@ -35,6 +36,94 @@ namespace BlazorDemoSap1.Client.Pages
             }
         };
 
+        // instance unique pour l'affichage dans la page
+        public Product? product { get; set; } = new()
+        {
+            Id = 1,
+            Name = "product 1",
+            Description = "description product 1",
+            Quantity = 10,
+            Category = "Shoes",
+            Price = 175.99
+        };
 
+        #region Binding two way lors d' un evenement
+        // Declaration de deux variables une pour recuperation une pour affichage
+        public string messageTwoWay { get; set; } = "hello world";
+        public string messageAfterInput { get; set; }
+
+        // methode qui permet de recupéré la valeur de l'input pour l'envoyer dans une autre variable
+        public void GetMessage()
+        {
+            messageAfterInput = messageTwoWay;
+        }
+        #endregion
+
+        #region 
+
+        public List<string> valueSelect { get; set; } = new()
+        {
+            "un",
+            "deux",
+            "trois"
+        };
+        public string  message1 { get; set; }
+        public void GetMessage(string value)
+        {
+            message1 = value;
+        }
+
+
+        #endregion
+
+        #region mini login
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string ResultLogin { get; set; }
+
+        public void Login()
+        {
+            ResultLogin = $" {Email} - {Password}";
+        }
+        #endregion
+
+        #region Exemple de binding
+        public List<string> values { get; set; } = new()
+        {
+            "value 11",
+            "value 12",
+            "value 13",
+            "value 14",
+        };
+
+        public List<string> resultFilter { get; set; } = new();
+
+        public string search { get; set; }
+
+        public void Filter(string value)
+        {
+            resultFilter = values.Where(s => s.Contains(value)).ToList();
+        }
+        #endregion
+
+        #region Communication parent enfant
+
+        // Valeur envoyé à l'enfant
+        public string valueParent{ get; set; }
+
+        // valeur reçu de l'enfant pour affichage
+        public string valueFromChild { get; set; }
+
+
+        // Methode pour recuperer la valeur de l'enfant
+        public void UpdateValueParent(string valueForUpdate)
+        {
+            valueFromChild = valueForUpdate;
+        }
+
+
+
+        #endregion
     }
 }
