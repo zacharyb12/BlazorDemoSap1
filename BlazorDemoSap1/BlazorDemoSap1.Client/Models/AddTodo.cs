66e99169cb313fd877f109adc59dc076f93638b1
zacharyb12 <boualemzachary89@yahoo.fr>
2026-03-18 14:51:59 +0100

    correction exo 2

---
+++ BlazorDemoSap1/BlazorDemoSap1.Client/Models/AddTodo.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorDemoSap1.Client.Models
+{
+    public class AddTodo
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ BlazorDemoSap1/BlazorDemoSap1.Client/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorDemoSap1.Client.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
