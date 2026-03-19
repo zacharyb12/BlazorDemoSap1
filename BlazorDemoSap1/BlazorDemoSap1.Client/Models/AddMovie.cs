d170b02a0076192bcabd773ef34ab57f04040b3c
zacharyb12 <boualemzachary89@yahoo.fr>
2026-03-19 11:27:32 +0100

    Correction Formulaires

---
+++ BlazorDemoSap1/BlazorDemoSap1.Client/Models/AddMovie.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDemoSap1.Client.Models
+{
+    public class AddMovie
+    {
+        [Required(ErrorMessage = "Le titre est requis.")]
+        [MaxLength(50, ErrorMessage = " La taille du titre ne peut pas dépasser 50 caractères")]
+        public string Titre { get; set; }
+
+
+        [Required(ErrorMessage = "La description est requise.")]
+        [MaxLength(200, ErrorMessage = " La taille de la description ne peut pas dépasser 200 caractères")]
+        public string Description { get; set; }
+
+
+        [Required(ErrorMessage = "L'année de production est requise.")]
+        [Range(1850,2026, ErrorMessage = " L'année de production doit être entre 1850 et 2026")]
+        public int AnneeDeProd { get; set; }
+
+
+        [Required(ErrorMessage ="Le nom du réalisateur est requis")]
+        [MaxLength(50, ErrorMessage = " La taille du nom du réalisateur ne peut pas dépasser 50 caractères")]
+        public string Realisateur { get; set; }
+    }
+}
---
+++ BlazorDemoSap1/BlazorDemoSap1.Client/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+namespace BlazorDemoSap1.Client.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public string  Description{ get; set; }
+        public int  AnneeDeProd { get; set; }
+        public string Realisateur { get; set; }
+    }
+}
